2a415227e802b63d1c5a342943f1d2b7dde5af13
arty <long151103@gmail.com>
2024-11-13 22:19:03 +0700

    data uploads

---
+++ src/main/java/com/connorng/ReUzit/controller/User/UserController.java
@@ -0,0 +1,32 @@
+package com.connorng.ReUzit.controller;
+
+import com.connorng.ReUzit.model.User;
+import com.connorng.ReUzit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @GetMapping
+    public List<User> getAllUsers () {
+        return userService.getAllUsers();
+    }
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
+        // Return the user details that are already authenticated and extracted from the JWT
+        return ResponseEntity.ok(userDetails);
+    }
+}
