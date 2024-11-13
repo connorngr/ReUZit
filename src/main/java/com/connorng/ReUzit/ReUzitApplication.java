b1b7e128e6b77524da936d96878c333ed7fdc3d3
PHLArty <long151103@gmail.com>
2024-11-13 21:34:33 +0700

    test push ubuntu

--- src/main/java/com/connorng/ReUzit/ReUzitApplication.java
+++ src/main/java/com/connorng/ReUzit/ReUzitApplication.java
@@ -35,8 +35,6 @@ public class ReUzitApplication {
 	@Bean
 	public CommandLineRunner runner(S3Service s3Service, S3Buckets s3Buckets) {
 		return args -> {
-//			testS3Buckets(s3Service, s3Buckets);
-//			createSampleListing();
 		};
 	}
 
@@ -48,49 +46,6 @@ public class ReUzitApplication {
 		System.out.println("hooray: " + new String(obj));
 	}
 
-//	private MultipartFile createMultipartFileFromUrl(String imageUrl) throws IOException {
-//		// Đọc nội dung hình ảnh từ URL
-//		InputStream inputStream = new URL(imageUrl).openStream();
-//		byte[] imageContent = inputStream.readAllBytes();
-//
-//		// Tạo MultipartFile từ nội dung hình ảnh
-//		return new CustomMultipartFile(
-//				"image",
-//				"sample-image.jpg", // Bạn có thể thay đổi tên file tùy ý
-//				"image/jpeg", // Thay đổi content type nếu hình ảnh là khác
-//				imageContent
-//		);
-//	}
 
-	private void createSampleListing() {
-		ListingRequest listingRequest = new ListingRequest();
-		listingRequest.setTitle("Sample Listing");
-		listingRequest.setDescription("This is a sample description for a listing.");
-		listingRequest.setPrice(100.0);
-		listingRequest.setCondition("new");
-		listingRequest.setStatus("active");
-		listingRequest.setUserId(1L);  // Replace with a valid user ID
-		listingRequest.setCategoryId(1L);  // Replace with a valid category ID
-
-//		try {
-			// Nếu muốn sử dụng hình ảnh từ URL
-			String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/b/ba/Flower_jtca001.jpg";
-
-			// Tạo MultipartFile từ URL
-//			MultipartFile imageFromUrl = createMultipartFileFromUrl(imageUrl);
-
-			// Tạo listing và thêm hình ảnh vào đó
-//			Listing createdListing = listingService.createListing(
-//					listingRequest,
-//					"long15@gmail.com", // Email hợp lệ
-//					Collections.singletonList(imageFromUrl) // Sử dụng danh sách chứa MultipartFile
-//			);
-
-			// Xuất thông tin listing đã tạo
-//			System.out.println("Created Listing: " + createdListing);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-	}
 
 }
